95570e17bf10f567095962224df6126373b21da7
Ryan Miller <ryan@devopsmachine.com>
2018-07-13 10:46:51 -0400

    Update version number
    Getting ready for release

--- setup.py
+++ setup.py
@@ -2,14 +2,14 @@ from distutils.core import setup
 setup(
     name='encase',
     packages=['encase'],
-    version='1.0.2',
+    version='1.1',
     description='A Better Dictionary Class',
     long_description=open('README.rst').read(),
     author='Ryan Miller',
     author_email='ryan@devopsmachine.com',
     license='MIT',
     url='https://github.com/RyanMillerC/encase',
-    download_url='https://github.com/RyanMillerC/encase/archive/1.0.2.tar.gz',
+    download_url='https://github.com/RyanMillerC/encase/archive/1.1.tar.gz',
     keywords=['data', 'dictionary'],
     classifiers=[
         'Intended Audience :: Developers',
